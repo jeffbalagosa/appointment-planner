6797e2c6c501b214632b67ac9c010fe673ae83b9
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-12-07 18:06:52 -0800

    Add ContactFormComponent with props.

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import ContractForm from "../../components/contactForm/ContactForm";
+import ContractForm, {
+  ContactForm,
+} from "../../components/contactForm/ContactForm";
 import TileList from "../../components/tileList/TileList";
 
 export const ContactsPage = ({ contacts, addContact }) => {
@@ -42,7 +44,16 @@ export const ContactsPage = ({ contacts, addContact }) => {
   return (
     <div>
       <section>
-        <h2 handleSubmit={handleSubmit}>Add Contact</h2>
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
