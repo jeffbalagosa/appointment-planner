0bcedd56414487f4f772add62061196bbb0b3177
jeffbalagosa <jeff.balagosa@gmail.com>
2022-12-10 17:19:43 -0800

    Retrieve prop object. Iterate over the values in the object, passed in via props, and render a <p> element for each value. Give a className of tile-title to the first <p> element. Give a className of tile to all other <p> elements

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(props.object).map((value, index) => (
+        <p key={index}>{value}</p>
+      ))}
     </div>
   );
 };
