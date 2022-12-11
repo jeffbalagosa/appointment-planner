6623c49849ddfc6886b3449687fa8dd9f4003053
jeffbalagosa <jeff.balagosa@gmail.com>
2022-12-10 17:04:34 -0800

    Import tile component.  Use map method.

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tile } from "./../tile/Tile.js";
 
-export const TileList = () => {
+export const TileList = (props) => {
   return (
     <div>
-      
+      {props.list.map((object, index) => (
+        <Tile object={object} key={index} />
+      ))}
     </div>
   );
 };
