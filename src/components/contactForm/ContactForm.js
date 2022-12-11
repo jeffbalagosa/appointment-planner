1feb25b537b3c885e31136b76382d0e67d5971d9
jeffbalagosa <jeff.balagosa@gmail.com>
2022-12-10 16:15:02 -0800

    Add handlers.  Build out form and inputs in render.

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,15 +1,40 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = (props) => {
+  const handleNameChange = (event) => {
+    props.setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    props.setEmail(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    props.setPhone(event.target.value);
+  };
+
   return (
-    ContactForm
+    <form onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={props.name}
+        onChange={handleNameChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={props.email}
+        onChange={handleEmailChange}
+      />
+      <input
+        type="tel"
+        placeholder="Phone"
+        value={props.phone}
+        onChange={handlePhoneChange}
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+      />
+      <button type="submit">Add Contact</button>
+    </form>
   );
 };
