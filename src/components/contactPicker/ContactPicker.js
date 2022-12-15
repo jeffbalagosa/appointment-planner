2eff73104e243bc6e7f82f0d6467c1c5be67780d
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-12-14 21:03:22 -0800

    Build contact picker component to requirements spec.

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
+  const { contacts, onChange } = props;
   return (
-    ContactPicker
+    <select onChange={onChange}>
+      <option value="" selected="selected">
+        No Contact Selected
+      </option>
+      {contacts.map((contact) => (
+        <option key={contact.name} value={contact.name}>
+          {contact.name}
+        </option>
+      ))}
+    </select>
   );
 };
